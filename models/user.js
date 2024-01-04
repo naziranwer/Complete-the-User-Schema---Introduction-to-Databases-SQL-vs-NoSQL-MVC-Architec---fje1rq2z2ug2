9401c71c6950659b322b2d14efb9ccfee5d1ca9e
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2024-01-04 21:58:58 +0530

    update

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
 var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
+const userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    pswd: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['buyer', 'seller'],
+      required: true
+    }
+  });
 
 module.exports =mongoose.model("users",userSchema);
